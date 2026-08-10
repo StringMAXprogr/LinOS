763accecee91ce4f498ad288be2c4156d004cbc0
StringMAX <max@kulturberatung.at>
2026-08-10 22:23:40 +0200

    Write 'LINOS' to video memory on startup
    Initialize video memory to display 'LINOS' on screen.

--- src/kernel/Main.rs
+++ src/kernel/Main.rs
@@ -5,6 +5,25 @@ use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let video_memory = 0xb8000 as *mut u8;
+
+    unsafe {
+        *video_memory = b'L';
+        *video_memory.add(1) = 0x0F;
+
+        *video_memory.add(2) = b'I';
+        *video_memory.add(3) = 0x0F;
+
+        *video_memory.add(4) = b'N';
+        *video_memory.add(5) = 0x0F;
+
+        *video_memory.add(6) = b'O';
+        *video_memory.add(7) = 0x0F;
+
+        *video_memory.add(8) = b'S';
+        *video_memory.add(9) = 0x0F;
+    }
+
     loop {
         core::hint::spin_loop();
     }
